f723d461536bdf037a749f99f53931ac94982464
sujeetsahu-cedcommerce <sujeetsahu@cedcoss.com>
2022-07-14 15:59:19 +0530

    Q4. Using class components create a username list as shown.

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import './App.css';
 import './style.css'
 import ListData from './ListData';
 import Products from "./Products";
+import UserNameList from './UserNameList';
 function App() {
   return (
     <div className="">
       <ListData/>
-      <Products/>
+     <h3>Q2. Make a Products Table which should work as:  1. Fill form and submit values. 2. Product should add in product list and display in list below add product form.</h3>
+      <Products/><br/><br/><br/>
+      <h3>Q4. Using class components create a username list as shown.</h3>
+      <UserNameList/>
     </div>
   );
 }
---
+++ src/UserNameList.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+
+export default class UserNameList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        input_value:"",
+        item:[],
+    };
+  }
+  
+  Take_Value = (event) => {
+      this.setState({
+        input_value:event.target.value,
+      })
+
+  }
+
+  Display_item = () => {
+    if(this.state.input_value!==""){
+      this.setState({
+        item:[...this.state.item, this.state.input_value],
+        input_value:"",
+      })
+    }
+  }
+  
+  Delete_Item = (event) => {
+   let index = event.target.id;
+   this.state.item.splice(index,1);
+   this.setState({
+     item: this.state.item,
+   })
+  }
+
+  render() {
+    return (
+      <div className="main_box">
+        <div className="sub_Box1">
+          <input type="text" value={this.state.input_value} placeholder="Enter New Items"onChange={this.Take_Value} style={{height:"38px"}}/>
+          <button onClick={this.Display_item}>Add</button>
+        </div>
+        <div className="sub_Box2">
+            {
+              this.state.item.map((item,index) => 
+              <div className="sub_sub_Box1">
+                <button id={index} style={{marginRight:"20px",color:"blue", backgroundColor:"blanchedalmond",border:"none",height:"1vh"}} onClick={this.Delete_Item}>X</button>
+                <span id={index}>{item}</span><br/>
+              </div>)
+            }
+        </div>
+      </div>
+    );
+  }
+}
